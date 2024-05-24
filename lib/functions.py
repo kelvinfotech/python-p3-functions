eb81901dac7e5487ea6042f6bf743f3891f1fcb1
Mwenda <kelvinfotech@gmail.com>
2024-05-24 12:50:08 +0300

    Python P3 Functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,36 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    if not isinstance(number, (int, float)):
+        return None
+    return number / 2
+
+greet_programmer()
+
+greet("Naureen")
+
+greet_with_default()
+
+greet_with_default("Sunny")
+
+sum_result = add(1, 2)
+
+print(sum_result)
+
+result = halve(4)
+print(result)
+
+result = halve("two")
+print(result)
